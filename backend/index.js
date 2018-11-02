c005be2a093bffaaf8cd1d4d8dab0f82abab3dbf
linux08 <abimbola130@gmail.com>
2018-11-02 23:10:11 +0100

    Code clean up

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,34 @@
+require('dotenv').config({ silent: true });
+
 const express = require('express');
+
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000;
 const routes = require('./routes');
 
+
+const DBURL = !process.env.DB_URL
+    ? 'mongodb://127.0.0.1:27017/dapp'
+    : process.env.DB_URL;
+//= ============================================================================
+/**
+ * database config
+ */
+//= ============================================================================
+
+// Connect to our Database and handle an bad connections
+mongoose.connect(
+    DBURL,
+    { useNewUrlParser: true },
+);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.once('connected', () => console.log(`Successfully connected to ${DBURL}`));
+mongoose.connection.on('error', (err) => {
+    console.log(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
 
 
 app.use(cors());
--- backend/routes.js
+++ backend/routes.js
@@ -20,25 +20,3 @@ router.post('/upload', Image.upload.single('file'), Image.uploadFile, Image.post
 router.get('/getfile/:hash', Image.getFile);
 
 module.exports = router;
-const express = require('express');
-
-const router = express.Router();
-const Image = require('./controllers/images');
-
-router.get('/', Image.test);
-router.get('/accounts', Image.accounts);
-router.get('/web3-status', Image.webStatus);
-
-router.get('/files/:name', Image.getData);
-
-router.get('/images', Image.all);
-router.get('/images/:label', Image.findByLabel);
-// find image by id
-router.get('/images-id/:id', Image.findById);
-
-/*  upload POST endpoint */
-router.post('/upload', Image.upload.single('file'), Image.uploadFile, Image.postData, Image.create);
-
-router.get('/getfile/:hash', Image.getFile);
-
-module.exports = router;
